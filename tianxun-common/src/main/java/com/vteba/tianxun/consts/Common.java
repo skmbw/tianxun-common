9a1dba96b38781f22cf1d2f9b8bdc60ac1e5da5e
skmbw <tongku2008@126.com>
2016-09-28 10:49:21 +0800

    定义几个常量

--- tianxun-common/src/main/java/com/vteba/tianxun/consts/Common.java
+++ tianxun-common/src/main/java/com/vteba/tianxun/consts/Common.java
@@ -1,10 +1,28 @@
 package com.vteba.tianxun.consts;
 
+/**
+ * 一般的常亮定义
+ * 
+ * @author yinlei
+ * @since 2016年9月28日 上午10:48:41
+ */
 public interface Common {
+	
 	/** 没有文章来源 */
 	public static final String NO_SRC = "-1";
+	
 	/** 平台作者id */
 	public static final String AUTHOR_ID = "system";
+	
 	/** 平台作者名 */
 	public static final String AUTHOR_NAME = "天寻";
+	
+	/** session中登录的验证码code */
+	public static final String LOGIN_CODE = "login_code";
+	
+	/** session中注册的验证码code */
+	public static final String REGIST_CODE = "regist_code";
+	
+	/** session中提问的验证码code */
+	public static final String ASK_CODE = "ask_code";
 }
