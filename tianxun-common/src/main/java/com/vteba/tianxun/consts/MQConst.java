df63ae2f1817e99a087cc8797cf8d70496500d0d
skmbw <tongku2008@126.com>
2016-09-29 17:29:35 +0800

    增加几个mq的tag常量

--- tianxun-common/src/main/java/com/vteba/tianxun/consts/MQConst.java
+++ tianxun-common/src/main/java/com/vteba/tianxun/consts/MQConst.java
@@ -7,5 +7,18 @@ public interface MQConst {
 	/** 用户注册tag */
 	public static final String TAG_REG = "Regist";
 	
+	/** 用户登录tag */
+	public static final String TAG_LOG = "Login";
 	
+	/** 发表文章tag */
+	public static final String TAG_PUBART = "publish_article";
+	
+	/** 用户提问tag */
+	public static final String TAG_ASK = "ask";
+	
+	/** 回答问题tag */
+	public static final String TAG_ANSWER = "answer";
+	
+	/** 评论tag */
+	public static final String TAG_COMMENT = "comment";
 }
