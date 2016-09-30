3327cad8be0ae09302cf2a89830eb9330abc0582
skmbw <tongku2008@126.com>
2016-09-30 14:08:12 +0800

    常量

--- tianxun-common/src/main/java/com/vteba/tianxun/consts/MQConst.java
+++ tianxun-common/src/main/java/com/vteba/tianxun/consts/MQConst.java
@@ -1,17 +1,26 @@
 package com.vteba.tianxun.consts;
 
+/**
+ * rocketmq中消息topic和tag常量
+ * 
+ * @author yinlei
+ * @since 2016年6月30日 上午10:55:06
+ */
 public interface MQConst {
-	/** 用户Topic */
+	/** 用户user Topic */
 	public static final String USER = "User";
 	
-	/** 积分Topic */
-	public static final String SCORE = "score";
+	/** 积分score Topic */
+	public static final String SCORE = "Score";
+	
+	/** 账户account Topic */
+	public static final String ACCOUNT = "Account";
 	
 	/** 用户注册tag */
 	public static final String TAG_REG = "Regist";
 	
 	/** 用户登录tag */
-	public static final String TAG_LOG = "Login";
+	public static final String TAG_LOG = "login";
 	
 	/** 发表文章tag */
 	public static final String TAG_PUBART = "publish_article";
